7d3f39538c156400daffb9d6644bc2de007c61a5
Bruno Ignacio Bordón <118336756+bruno10-0@users.noreply.github.com>
2024-11-06 16:59:31 -0300

    Agregando URL de api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.100.141:3000/api",
+  baseURL: "https://backend-node-w3e9.onrender.com/api",
   withCredentials: true,
 });
 
